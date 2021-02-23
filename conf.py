0b908f88f13cb2e6176cd31b55c1a26baa419cff
Niharikaterli <79441089+Niharikaterli@users.noreply.github.com>
2021-02-23 16:48:51 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'git and github documentation'
+copyright = '2021, niharika'
+author = 'niarika'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
